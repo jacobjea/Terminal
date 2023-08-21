c7c6172cea1805388aeb7c1ab86dea3df5c92c66
jacobjea <jsw992@gmail.com>
2023-08-21 15:36:41 +0900

    export package용 폴더 정리, 패키지 추가

--- Assets/Terminal System/TerminalFunc.cs
+++ Assets/Terminal System/TerminalFunc.cs
@@ -26,7 +26,7 @@ public class TerminalFunc : MonoBehaviour
         DontDestroyOnLoad(gameObject);
 
         // 터미널 생성 UI 생성
-        TerminalSystem.UI_Termianl = Instantiate(Resources.Load<GameObject>("Terminal/UI_Terminal"), transform).GetComponent<UI_Terminal>();
+        TerminalSystem.UI_Termianl = Instantiate(Resources.Load<GameObject>("UI_Terminal"), transform).GetComponent<UI_Terminal>();
         TerminalSystem.UI_Termianl.gameObject.SetActive(false);
 
         // 함수이름으로 등록
